b7519ad22a4108a5bc8d5c0b4de8d4f5f24b812c
Ilkin Ibadov <ilkin.ibadov21@gmail.com>
2025-04-20 09:51:41 +0400

    changed key

--- app/[lng]/about/page.js
+++ app/[lng]/about/page.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import { useTranslation } from 'lib/i18n'
 import LanguageSelect from 'components/LanguageSelect'
 
-
 export default async function Page({ params }) {
     const { lng } = await params
     const { t } = await useTranslation(lng, 'about')
